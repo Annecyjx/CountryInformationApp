6033e68ffed05c351a6f122c60e7b5f347b58ee8
Xuan Jiang <annecyjx@gmail.com>
2017-01-27 14:56:40 +0100

    final

--- module/app.js
+++ module/app.js
@@ -1,24 +1,13 @@
 //I/O practice country app
-//Part 1
-var countryName = process.argv[2];
-
-var fs = require('fs'); 
-
-fs.readFile('./countries.json' ,'utf-8',function(error,data){
-	if (error) {
-		console.log('failed to read this file.');
-		throw error;
-	}
-	console.log ('Country:'+''+ countryName);
-
-	var countries = JSON.parse(data)
-
+//Part 2
+var readandparse = require('./json-file-reader.js')
+var file = 'countries.json'
+console.log ('Country:'+''+ process.argv[2]);
+readandparse(file, function(countries){
 	for (var i = 0; i < countries.length; i++){
-		if (countries[i].name == countryName){
+		if (countries[i].name === process.argv[2]){
 			console.log ('Top Level Domain:'+''+ countries[i].topLevelDomain);
 		}
 	}
-
 })
-
 
--- module/json-file-reader.js
+++ module/json-file-reader.js
@@ -1,8 +1,6 @@
-
-// var filename = require('./countries.json')
+// can read and parse any json file
 
 function read(filename, callback){
-    // var filename ='countries.json';
 	var fs = require('fs'); 
 	fs.readFile('./' + filename ,'utf-8',function(error,data){
 		if (error) {
@@ -11,29 +9,7 @@ function read(filename, callback){
 		}
 		var parsed = JSON.parse(data)
 		callback(parsed);
-
 	})
 }
 module.exports= read
-// var readandparse = function(countryName){
-// 	var fs = require('fs'); 
-
-// 	fs.readFile('./countries.json' ,'utf-8',function(error,data){
-// 		if (error) {
-// 			console.log('failed to read this file.');
-// 			throw error;
-// 		}
-// 		console.log ('Country:'+''+ countryName);
-// 	//parse it
-// 	var countries = JSON.parse(data)
-
-// 	for (var i = 0; i < countries.length; i++){
-// 		if (countries[i].name == countryName){
-// 			console.log ('Top Level Domain:'+''+ countries[i].topLevelDomain);
-// 		}
-// 	}
-
-// })
-	
-// }
 
